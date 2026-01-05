07e0a23e0daf206549e4a74fafe9629ebef88c25
michaelmagdy15 <michaelmitry13@gmail.com>
2026-01-05 04:53:32 +0200

    5678

--- modern/src/components/Hero.tsx
+++ modern/src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import { motion, useScroll, useTransform, Variants } from 'framer-motion';
+import { motion, useScroll, useTransform, type Variants } from 'framer-motion';
 
 const Hero = () => {
     const { scrollY } = useScroll();
